9e9fe5ab1fe0acfc8e36fde6aa3821d25f70e999
Jeisson <jeda1989@gmail.com>
2024-02-03 19:41:56 -0500

    Se incorpora funcionalidad de login con sus validaciones

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,48 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 import { Grid, Card, CardContent, TextField, Button  } from '@mui/material';
 import Swal from 'sweetalert2';
 
 const Login = () => {
 
-    const [user, setUser] = useState('Jeisson');
+    const navigate = useNavigate();
 
-    const onLogin = () => {
-        setUser(user)
-        Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: `Bienvenido ${user}`,
-            showConfirmButton: false,
-            timer: 1500
-          });
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('')
+
+    const onLogin = async () => {
+
+        if (!email || !password) {
+            Swal.fire({
+                icon: 'warning',
+                text: 'Por favor, complete todos los campos.',
+                confirmButtonColor: '#3498db'
+            });
+            return;
+        }
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `Bienvenido ${userCredential.user.displayName}`,
+                showConfirmButton: false,
+                timer: 2000
+            }); 
+            navigate('/Home');  
+            } catch (error) {
+                if (error.code === 'auth/invalid-credential') {
+                    Swal.fire({
+                        icon: 'error',
+                        text: 'Correo electrónico o contraseña incorrectos.',
+                        confirmButtonColor: '#3498db'
+                    });        
+                } 
+            console.log('Error en el inicio de sesión:', error);
+        }    
     }
 
     return (
@@ -24,14 +51,14 @@ const Login = () => {
             <Grid container spacing={2} justifyContent="center" alignItems="flex-start" style={{ marginTop: 50 }}>
                 <Card elevation={3} style={{ backgroundColor: '#e6f7ff', width: '80%', maxWidth: 400 }}>
                     <CardContent>
-                        <TextField id="outlined-email" label="Correo" variant="outlined"  fullWidth style={{ marginBottom: 15 }}/>
-                        <TextField id="outlined-pass" type="password" label="Contraseña" variant="outlined" fullWidth />
+                        <TextField id="outlined-email" label="Correo" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value) } fullWidth style={{ marginBottom: 15 }}/>
+                        <TextField id="outlined-pass" type="password" label="Contraseña" value={password}  onChange={(e) => setPassword(e.target.value)} variant="outlined" fullWidth />
                         <Link to="/Register">
                             <Button variant="contained" color="primary" style={{ width: '100%', marginTop: 10 }}>
                                 Registrarse
                             </Button>
                         </Link>
-                        <Link to="/Home">
+                        <Link>
                             <Button variant="contained" color="primary"  onClick={onLogin} style={{ width: '100%', marginTop: 10 }}>
                                 Iniciar Sesión
                             </Button>
